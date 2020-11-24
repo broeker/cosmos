c0d65ff926a9a3c1e33f9599498aa2326686c18e
broeker <tim@electriccitizen.com>
2020-11-23 18:20:28 -0600

    preview test

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -6,15 +6,16 @@ CMS.registerPreviewTemplate("blog", Post);
 CMS.registerPreviewTemplate("pages", Page);
 
 CMS.registerPreviewStyle("/_includes/assets/css/inline.css");
+
 // Register any CSS file on the home page as a preview style
-fetch("/")
-  .then(response => response.text())
-  .then(html => {
-    const f = document.createElement("html");
-    f.innerHTML = html;
-    Array.from(f.getElementsByTagName("link")).forEach(tag => {
-      if (tag.rel == "stylesheet" && !tag.media) {
-        CMS.registerPreviewStyle(tag.href);
-      }
-    });
-  });
+// fetch("/")
+//   .then(response => response.text())
+//   .then(html => {
+//     const f = document.createElement("html");
+//     f.innerHTML = html;
+//     Array.from(f.getElementsByTagName("link")).forEach(tag => {
+//       if (tag.rel == "stylesheet" && !tag.media) {
+//         CMS.registerPreviewStyle(tag.href);
+//       }
+//     });
+//   });
