51602f9403f80bf9566f21223a5e555bb225c52a
broeker <tim@electriccitizen.com>
2020-11-23 19:09:56 -0600

    remove main block from preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -8,11 +8,9 @@ const Page = createClass({
     const entry = this.props.entry;
 
     return html`
-      <main>
         <h1>${entry.getIn(["data", "title"], null)}</h1>
 
         ${this.props.widgetFor("body")}
-      </main>
     `;
   }
 });
